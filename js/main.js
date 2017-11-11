e9378d5efe95d095e5e034eccfc57e3ec134ccac
Sean Darcy <darcys22@gmail.com>
2017-11-11 23:33:37 +1100

    has a basic calculation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+var estimator = new Estimator();
+
 $('#TaxableIncomeInput').on('input', function () {
     window.taxableIncome = stripwhitecommas($(this).val());
+    window.FY = stripwhitecommas($('#FinancialYearInput').val());
     calculate();
 });
 $('#FinancialYearInput').on('input', function () {
+    window.FY = stripwhitecommas($(this).val());
     calculate();
 });
 $('#settingsModal').on('hidden.bs.modal', function () {
@@ -14,6 +18,29 @@ function editSettings() {
 
 function calculate() {
   $("#TaxableIncomeOutput").html(moneyNumber(window.taxableIncome));
+  var payableObject = estimator.calculateTaxPayable(window.taxableIncome, window.FY);
+  $("#TaxPayableVal").html(moneyNumber(payableObject.taxOnIncome));
+  $("#MedicareVal").html(moneyNumber(payableObject.medicareLevy));
+  $("#MLSVal").html(moneyNumber(payableObject.medicareLevySurcharge));
+  $("#LITOVal").html(moneyNumber(payableObject.offsets.lowIncomeTaxOffset));
+
+  var sumPayable = payableObject.taxOnIncome + payableObject.medicareLevy + payableObject.medicareLevySurcharge
+  var sumOffsets = payableObject.offsets.lowIncomeTaxOffset
+  var amountPayable = sumPayable - sumOffsets;
+  $("#SumOffset").html(moneyNumber(sumOffsets));
+  $("#SumPayable").html(moneyNumber(sumPayable));
+  $("#PayableVal").html(moneyNumber(amountPayable));
+
+  //TODO(Sean):Temporary budger repair levy
+  //TODO(Sean):Excess private health reduction or refund
+  //TODO(Sean):Financial supplement repayment
+  //TODO(Sean):Trade Support Loan Repayment
+  //TODO(Sean):Franking Credits
+  //TODO(Sean):Foreign resident withholding credits
+  //TODO(Sean):Seniors and Pensioners (SAPTO)
+  //TODO(Sean):Superannuation Offset
+  //TODO(Sean):Private health insurance
+  
 }
 
 function stripwhitecommas(str) {
@@ -33,7 +60,6 @@ function formatcomma(element) {
 }
 
 function main() {
-  var estimator = new Estimator();
   window.now = moment();
   if (window.now.month() < 6) {
     window.now.set('year', now.year() -1);
@@ -45,5 +71,8 @@ function main() {
   $("#FinancialYearInput").val(window.endFY.format("YYYY"));
   $("#FinancialYearInput").attr({"max": window.endFY.format("YYYY")});
 
+  window.taxableIncome = 50000;
+  window.FY = 2017;
+  calculate();
 }
 main();
